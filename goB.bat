7d7e562230cf64430bb1e50e8f683d8f9667cffb
Yih Chuang <yih.chuang@gmail.com>
2014-09-11 12:33:07 -0700

    version 09/11

---
+++ goB.bat
@@ -0,0 +1,1 @@
+uefiBuild.py -b C:\\uEFI_build\\ini\\build0911B.ini -s C:\\uEFI_build\\ini\\sysemInfo.ini 
---
+++ goG.bat
@@ -0,0 +1,1 @@
+uefiBuild.py -b C:\\uEFI_build\\ini\\build0911G.ini -s C:\\uEFI_build\\ini\\sysemInfo.ini 
